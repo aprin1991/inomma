356420897d13b1073d6263470956768090d585ea
Aprin <ostovari.eng@gmail.com>
2022-03-26 20:38:27 +0430

    edit

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "redux/store";
 import { PersistGate } from "redux-persist/integration/react";
-
+//thisis
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
